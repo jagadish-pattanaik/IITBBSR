7509cc881aaddd319cd5a7a5641172ea420d06c6
jagadish-pattanaik <jaguweb1234@gmail.com>
2024-12-07 15:49:52 +0530

    fix deploy vercel

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -8,9 +8,18 @@ const requiredEnvVars = [
 ];
 
 export const validateFirebaseConfig = () => {
-  const missingVars = requiredEnvVars.filter(
-    varName => !process.env[varName]
-  );
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID || process.env.FIREBASE_APP_ID
+  };
+
+  const missingVars = Object.entries(config)
+    .filter(([_, value]) => !value)
+    .map(([key]) => key);
 
   if (missingVars.length > 0) {
     throw new Error(
@@ -18,12 +27,5 @@ export const validateFirebaseConfig = () => {
     );
   }
 
-  return {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+  return config;
 }; 
